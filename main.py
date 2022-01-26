f7f8da9529d8be7ab6761c3fac2b411330096729
Pavel <pavelc.git@posteo.net>
2022-01-25 16:42:24 -0800

    Start converting main.py to mujoco-py 2.1 and tf2. Unfinished!

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ def load_from_file(param_pkl_path):
 def setFromFlat(var_list, flat_params):
     shapes = list(map(lambda x: x.get_shape().as_list(), var_list))
     total_size = np.sum([int(np.prod(shape)) for shape in shapes])
-    theta = tf.placeholder(tf.float32, [total_size])
+    theta = tf.compat.v1.placeholder(tf.float32, [total_size])
     start = 0
     assigns = []
     for (shape, v) in zip(shapes, var_list):
         size = int(np.prod(shape))
-        assigns.append(tf.assign(v, tf.reshape(theta[start:start + size], shape)))
+        assigns.append(tf.compat.v1.assign(v, tf.reshape(theta[start:start + size], shape)))
         start += size
     op = tf.group(*assigns)
-    tf.get_default_session().run(op, {theta: flat_params})
+    tf.compat.v1.get_default_session().run(op, {theta: flat_params})
 
 def run(config):
     if config.env == "kick-and-defend":
@@ -50,10 +50,10 @@ def run(config):
 
     param_paths = config.param_paths
 
-    tf_config = tf.ConfigProto(
+    tf_config = tf.compat.v1.ConfigProto(
         inter_op_parallelism_threads=1,
         intra_op_parallelism_threads=1)
-    sess = tf.Session(config=tf_config)
+    sess = tf.compat.v1.Session(config=tf_config)
     sess.__enter__()
 
     policy = []
@@ -65,13 +65,13 @@ def run(config):
                                      ac_space=env.action_space.spaces[i],
                                      hiddens=[128, 128], normalize=True))
         else:
-            policy.append(MlpPolicyValue(scope=scope, reuse=False,
+            policy.append(MlpPolicyValue(sess=sess, n_env=1, n_steps=config.max_episodes, n_batch=1, scope=scope, reuse=False,
                                          ob_space=env.observation_space.spaces[i],
                                          ac_space=env.action_space.spaces[i],
                                          hiddens=[64, 64], normalize=True))
 
     # initialize uninitialized variables
-    sess.run(tf.variables_initializer(tf.global_variables()))
+    sess.run(tf.compat.v1.variables_initializer(tf.compat.v1.global_variables()))
 
     params = [load_from_file(param_pkl_path=path) for path in param_paths]
     for i in range(len(policy)):
@@ -83,13 +83,19 @@ def run(config):
     total_reward = [0.0  for _ in range(len(policy))]
     total_scores = [0 for _ in range(len(policy))]
     # total_scores = np.asarray(total_scores)
-    observation = env.reset()
+    observation = list(env.reset())
     print("-"*5 + " Episode %d " % (num_episodes+1) + "-"*5)
     while num_episodes < max_episodes:
         env.render()
-        action = tuple([policy[i].act(stochastic=True, observation=observation[i])[0]
-                        for i in range(len(policy))])
-        observation, reward, done, infos = env.step(action)
+        reward = []
+        done = []
+        infos = []
+        for i in range(len(policy)):
+            pol_observation, pol_reward, pol_done, pol_infos = policy[i].step(observation[i].reshape(1, -1))
+            observation[i] = pol_observation
+            reward.append(pol_reward)
+            done.append(pol_done)
+            infos.append(pol_infos)
         nstep += 1
         for i in range(len(policy)):
             total_reward[i] += reward[i]
